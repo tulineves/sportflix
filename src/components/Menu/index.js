77206539a1e0639bea8da4723e2b83bf9a4baed9
Arthur Neves <aneves@segware.com>
2020-07-29 00:18:36 -0300

    Updating project

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+
 
 //import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <a to="/">
                 <img className="Logo" src={Logo} alt="AluraFlix logo"/>
             </a>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
